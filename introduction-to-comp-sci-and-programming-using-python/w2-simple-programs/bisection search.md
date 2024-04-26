bb1ee73339f202a319b6c9d11a96e9fd03289aba
Luz <luzojeda@proton.me>
2024-04-26 19:49:13 -0300

    guess-my-number

---
+++ introduction-to-comp-sci-and-programming-using-python/w2-simple-programs/bisection search.md
@@ -0,0 +1,10 @@
+- Instead of guessing from the beginning, we pick a number in the middle between 1 and x
+- Example:
+	- Root of x lies between 1 and x.
+	- We pick a number between 1 and x
+	- Was guess too big or too small?
+	- If $g^2 > x$ then g is too big, we pick a number between 1 and g
+## convergence
+Guess converges to the answer on the order of $log_2N$ steps
+
+Greatly reduces computation time against guessing from 1 to x, checking every case according to the step
---
+++ introduction-to-comp-sci-and-programming-using-python/w2-simple-programs/guess-my-number.py
@@ -0,0 +1,24 @@
+n = input('Please think of a number between 0 and 100! ')
+
+numGuesses = 0
+
+low = 0
+high = int(n) + 1
+ans = (high + low) // 2
+
+while ans != n:
+    print("Is your secret number " + str(ans) + "?")
+    user_input = input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.")
+    
+    if user_input not in 'lch':
+        print("Sorry, I did not understand your input.")
+    else:
+        if user_input == 'h':
+            high = (high + low) // 2
+        elif user_input == 'c':
+            break
+        else:
+            low = (high + low) // 2
+        ans = (high + low) // 2
+
+print("Game over. Your secret number was: " + str(ans))
